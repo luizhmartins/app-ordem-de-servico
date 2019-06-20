4791a9475cca8ca36e74a0ea4657557eaec1260f
johnvcosta <jonathancosta123@gmail.com>
2019-06-20 01:43:16 -0300

    classe OrdemServico criada;

---
+++ app/src/main/java/com/example/appordemdeservico/OrdemServico.java
@@ -0,0 +1,64 @@
+package com.example.appordemdeservico;
+
+import java.util.Date;
+
+public class OrdemServico {
+
+    private int id;
+    private int idcliente;
+    private String tipoServico;
+    private Date dataServico;
+    private double valor;
+    private String descricao;
+
+    public OrdemServico() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(int idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public String getTipoServico() {
+        return tipoServico;
+    }
+
+    public void setTipoServico(String tipoServico) {
+        this.tipoServico = tipoServico;
+    }
+
+    public Date getDataServico() {
+        return dataServico;
+    }
+
+    public void setDataServico(Date dataServico) {
+        this.dataServico = dataServico;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
